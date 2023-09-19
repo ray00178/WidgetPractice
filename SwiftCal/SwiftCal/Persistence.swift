9d26139b6a7d61d75f031f34a68684ba0dd56b8e
Ray <ray00178@gmail.com>
2023-09-19 21:38:05 +0800

    - add(Project): Create App Group & Core Data Migration

--- SwiftCal/SwiftCal/Persistence.swift
+++ SwiftCal/SwiftCal/Persistence.swift
@@ -9,7 +9,19 @@ import CoreData
 
 struct PersistenceController {
   static let shared = PersistenceController()
-
+  
+  private let databaseName: String = "SwiftCal.sqlite"
+  
+  var oldStoreURL: URL {
+    let directory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+    return directory.appending(path: databaseName)
+  }
+  
+  var shareStoreURL: URL {
+    let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.tw.midnight.SwiftCal")!
+    return container.appending(path: databaseName)
+  }
+  
   static var preview: PersistenceController = {
     let result = PersistenceController(inMemory: true)
     let viewContext = result.container.viewContext
@@ -41,11 +53,41 @@ struct PersistenceController {
     if inMemory {
       container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
     }
+    else if FileManager.default.fileExists(atPath: oldStoreURL.path) == false {
+      container.persistentStoreDescriptions.first!.url = shareStoreURL
+    }
+    
+    print("Container URL = \(container.persistentStoreDescriptions.first!.url!)")
+    
     container.loadPersistentStores(completionHandler: { _, error in
       if let error = error as NSError? {
         fatalError("Unresolved error \(error), \(error.userInfo)")
       }
     })
+    
+    migrateStore(for: container)
     container.viewContext.automaticallyMergesChangesFromParent = true
+  }
+  
+  func migrateStore(for contianer: NSPersistentContainer) {
+    print ("Went into migrateStore")
+    let coodinator = contianer.persistentStoreCoordinator
+    
+    guard let oldStore = coodinator.persistentStore(for: oldStoreURL) else { return }
+    print("Old store no longer exists")
+    
+    do {
+      _ = try coodinator.migratePersistentStore(oldStore, to: shareStoreURL, type: .sqlite)
+      print("Migration successful")
+    } catch let error {
+      fatalError("Unable to migrate share store \(error.localizedDescription)")
+    }
+    
+    do {
+      try FileManager.default.removeItem(at: oldStoreURL)
+      print("Old store deleted")
+    } catch let error {
+      print("Unable to delete old store \(error.localizedDescription)")
+    }
   }
 }
---
+++ SwiftCal/SwiftCalWidget/SwiftCalWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftCalWidgetBundle.swift
+//  SwiftCalWidget
+//
+//  Created by Ray on 2023/9/18.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SwiftCalWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SwiftCalWidget()
+    }
+}
