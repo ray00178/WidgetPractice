b6849fbd53150f7fa3e8da377ef514d56d212073
Ray <ray00178@gmail.com>
2023-09-27 22:08:19 +0800

    - add(Project): Finish Intent Configuration

--- Monthly/MonthlyWidget/MonthlyWidget.swift
+++ Monthly/MonthlyWidget/MonthlyWidget.swift
@@ -10,25 +10,27 @@ import WidgetKit
 
 // MARK: - Provider
 
-struct Provider: TimelineProvider {
+struct Provider: IntentTimelineProvider {
   func placeholder(in _: Context) -> DayEntry {
-    DayEntry(date: Date())
+    DayEntry(date: Date(), showFunFont: false)
   }
 
-  func getSnapshot(in _: Context, completion: @escaping (DayEntry) -> Void) {
-    let entry = DayEntry(date: Date())
+  func getSnapshot(for _: ChangeFontIntent, in _: Context, completion: @escaping (DayEntry) -> Void) {
+    let entry = DayEntry(date: Date(), showFunFont: false)
     completion(entry)
   }
 
-  func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+  func getTimeline(for configuration: ChangeFontIntent, in _: Context, completion: @escaping (Timeline<DayEntry>) -> Void) {
     var entries: [DayEntry] = []
+
+    let showFunFont: Bool = configuration.funFont == 1
 
     // Generate a timeline consisting of seven entries a day apart, starting from the current date.
     let currentDate = Date()
     for dayOffset in 0 ..< 7 {
       let entryDate = Calendar.current.date(byAdding: .day, value: dayOffset, to: currentDate)!
       let startDate = Calendar.current.startOfDay(for: entryDate)
-      let entry = DayEntry(date: startDate)
+      let entry = DayEntry(date: startDate, showFunFont: showFunFont)
       entries.append(entry)
     }
 
@@ -39,8 +41,36 @@ struct Provider: TimelineProvider {
 
 // MARK: - DayEntry
 
+/* struct Provider: TimelineProvider {
+   func placeholder(in _: Context) -> DayEntry {
+     DayEntry(date: Date())
+   }
+
+   func getSnapshot(in _: Context, completion: @escaping (DayEntry) -> Void) {
+     let entry = DayEntry(date: Date())
+     completion(entry)
+   }
+
+   func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+     var entries: [DayEntry] = []
+
+     // Generate a timeline consisting of seven entries a day apart, starting from the current date.
+     let currentDate = Date()
+     for dayOffset in 0 ..< 7 {
+       let entryDate = Calendar.current.date(byAdding: .day, value: dayOffset, to: currentDate)!
+       let startDate = Calendar.current.startOfDay(for: entryDate)
+       let entry = DayEntry(date: startDate)
+       entries.append(entry)
+     }
+
+     let timeline = Timeline(entries: entries, policy: .atEnd)
+     completion(timeline)
+   }
+ } */
+
 struct DayEntry: TimelineEntry {
   let date: Date
+  let showFunFont: Bool
 }
 
 // MARK: - MonthlyWidgetEntryView
@@ -48,12 +78,13 @@ struct DayEntry: TimelineEntry {
 struct MonthlyWidgetEntryView: View {
   var entry: DayEntry
   var config: MonthConfig
-  
+  let funFontName: String = "Chalkduster"
+
   init(entry: DayEntry) {
     self.entry = entry
-    self.config = MonthConfig.determineConfig(from: entry.date)
+    config = MonthConfig.determineConfig(from: entry.date)
   }
-  
+
   var body: some View {
     ZStack {
       ContainerRelativeShape()
@@ -64,7 +95,7 @@ struct MonthlyWidgetEntryView: View {
           Text(config.emoji)
             .font(.title)
           Text(entry.date.formatted(.dateTime.weekday(.wide)))
-            .font(.title3)
+            .font(entry.showFunFont ? .custom(funFontName, size: 24) : .title3)
             .fontWeight(.bold)
             .minimumScaleFactor(0.6)
             .foregroundColor(config.weekdayColor)
@@ -72,7 +103,8 @@ struct MonthlyWidgetEntryView: View {
         }
 
         Text(entry.date.formatted(.dateTime.day(.twoDigits)))
-          .font(.system(size: 80, weight: .heavy))
+          .font(entry.showFunFont ? .custom(funFontName, size: 80) :
+                                    .system(size: 80, weight: .heavy))
           .foregroundStyle(config.dayColor)
       }
       .padding()
@@ -86,7 +118,11 @@ struct MonthlyWidget: Widget {
   let kind: String = "MonthlyWidget"
 
   var body: some WidgetConfiguration {
-    StaticConfiguration(kind: kind, provider: Provider()) { entry in
+    IntentConfiguration(
+      kind: kind,
+      intent: ChangeFontIntent.self,
+      provider: Provider()
+    ) { entry in
       if #available(iOS 17.0, *) {
         MonthlyWidgetEntryView(entry: entry)
           .containerBackground(.white.gradient, for: .widget)
@@ -108,6 +144,5 @@ struct MonthlyWidget: Widget {
 #Preview(as: .systemSmall) {
   MonthlyWidget()
 } timeline: {
-  DayEntry(date: .now)
-  DayEntry(date: .now)
+  DayEntry(date: .now, showFunFont: true)
 }
