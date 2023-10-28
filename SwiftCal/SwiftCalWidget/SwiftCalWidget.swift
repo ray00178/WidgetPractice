2a6612062d3d0960f6fdc79ca03b94be5072c647
Ray <ray00178@gmail.com>
2023-10-28 23:15:45 +0800

    - add(Project): Practice Swift Cal Lock Screen Widgets

--- SwiftCal/SwiftCalWidget/SwiftCalWidget.swift
+++ SwiftCal/SwiftCalWidget/SwiftCalWidget.swift
@@ -61,52 +61,24 @@ struct CalendarEntry: TimelineEntry {
 // MARK: - SwiftCalWidgetEntryView
 
 struct SwiftCalWidgetEntryView: View {
+  @Environment(\.widgetFamily) var widgetFamily
+
   var entry: CalendarEntry
 
-  var columns: [GridItem] = Array(repeating: GridItem(.flexible()), count: 7)
-
   var body: some View {
-    HStack {
-      Link(destination: URL(string: "streak")!) {
-        VStack {
-          Text("\(calculateStreakValue())")
-            .font(.system(size: 70, design: .rounded))
-            .bold()
-            .foregroundStyle(.orange)
-          Text("day streak")
-            .font(.caption2)
-            .foregroundStyle(.secondary)
-        }
-      }
-
-      Link(destination: URL(string: "calendar")!) {
-        VStack {
-          CalendarHeaderView(font: .caption)
-
-          LazyVGrid(columns: columns, spacing: 8) {
-            ForEach(entry.days) { day in
-              if day.date!.monthInt != Date().monthInt {
-                Text(" ")
-              } else {
-                Text(day.date!.formatted(.dateTime.day()))
-                  .font(.caption2)
-                  .bold()
-                  .frame(maxWidth: .infinity)
-                  .foregroundStyle(day.didStudy ? .orange : .secondary)
-                  .background {
-                    Circle()
-                      .foregroundStyle(.orange.opacity(day.didStudy ? 0.3 : 0.0))
-                      .scaleEffect(1.5)
-                  }
-              }
-            }
-          }
-        }
-      }
-
-      .padding(.leading, 5)
+    switch widgetFamily {
+    case .systemMedium:
+      MediumCalendarView(entry: entry, streakValue: calculateStreakValue())
+    case .accessoryInline:
+      Label("Streak \(calculateStreakValue()) days", systemImage: "swift")
+        .widgetURL(URL(string: "streak"))
+    case .accessoryCircular:
+      LockScreenCircularView(entry: entry)
+    case .accessoryRectangular:
+      LockScreenRectangularView(entry: entry)
+    default:
+      EmptyView()
     }
-    .padding()
   }
 
   /// 計算連續study幾日
@@ -152,7 +124,10 @@ struct SwiftCalWidget: Widget {
     .configurationDisplayName("Swift Study Calendar")
     .description("Track days you study Swift with streak.")
     .contentMarginsDisabled()
-    .supportedFamilies([.systemMedium])
+    .supportedFamilies([.systemMedium,
+                        .accessoryInline,
+                        .accessoryCircular,
+                        .accessoryRectangular])
   }
 }
 
@@ -160,4 +135,124 @@ struct SwiftCalWidget: Widget {
   SwiftCalWidget()
 } timeline: {
   CalendarEntry(date: .now, days: [])
+}
+
+#Preview(as: .accessoryRectangular) {
+  SwiftCalWidget()
+} timeline: {
+  CalendarEntry(date: .now, days: [])
+}
+
+// MARK: - MediumCalendarView
+
+private struct MediumCalendarView: View {
+  var entry: CalendarEntry
+
+  var streakValue: Int
+
+  var columns: [GridItem] = Array(repeating: GridItem(.flexible()), count: 7)
+
+  var body: some View {
+    HStack {
+      Link(destination: URL(string: "streak")!) {
+        VStack {
+          Text("\(streakValue)")
+            .font(.system(size: 70, design: .rounded))
+            .bold()
+            .foregroundStyle(.orange)
+          Text("day streak")
+            .font(.caption2)
+            .foregroundStyle(.secondary)
+        }
+      }
+
+      Link(destination: URL(string: "calendar")!) {
+        VStack {
+          CalendarHeaderView(font: .caption)
+
+          LazyVGrid(columns: columns, spacing: 8) {
+            ForEach(entry.days) { day in
+              if day.date!.monthInt != Date().monthInt {
+                Text(" ")
+              } else {
+                Text(day.date!.formatted(.dateTime.day()))
+                  .font(.caption2)
+                  .bold()
+                  .frame(maxWidth: .infinity)
+                  .foregroundStyle(day.didStudy ? .orange : .secondary)
+                  .background {
+                    Circle()
+                      .foregroundStyle(.orange.opacity(day.didStudy ? 0.3 : 0.0))
+                      .scaleEffect(1.5)
+                  }
+              }
+            }
+          }
+        }
+      }
+      .padding(.leading, 5)
+    }
+    .padding()
+  }
+}
+
+// MARK: - LockScreenCircularView
+
+private struct LockScreenCircularView: View {
+  var entry: CalendarEntry
+
+  var currentCalendarDays: Int {
+    entry.days.filter { $0.date?.monthInt == Date().monthInt }.count
+  }
+
+  var daysStudied: Int {
+    entry.days.filter { $0.date?.monthInt == Date().monthInt }
+              .filter(\.didStudy)
+              .count
+  }
+
+  var body: some View {
+    Gauge(
+      value: Double(daysStudied),
+      in: 0 ... Double(currentCalendarDays),
+      label: {
+        Image(systemName: "swift")
+      },
+      currentValueLabel: {
+        Text("\(daysStudied)")
+      }
+    )
+    .gaugeStyle(.accessoryCircular)
+  }
+}
+
+// MARK: - LockScreenRectangularView
+
+private struct LockScreenRectangularView: View {
+  var entry: CalendarEntry
+
+  var columns: [GridItem] = Array(repeating: GridItem(.flexible()), count: 7)
+
+  var body: some View {
+    LazyVGrid(columns: columns, spacing: 4) {
+      ForEach(entry.days) { day in
+        if day.date!.monthInt != Date().monthInt {
+          Text(" ")
+            .font(.system(size: 7))
+        } else {
+          if day.didStudy {
+            Image(systemName: "swift")
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 7, height: 7)
+          } else {
+            Text(day.date!.formatted(.dateTime.day()))
+              .font(.system(size: 7))
+              .frame(maxWidth: .infinity)
+          }
+        }
+      }
+    }
+    .padding()
+  }
 }
